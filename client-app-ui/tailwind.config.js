cb390d5d6bc926b0280c05355eefd76af0c78d36
Jason Liu <jasonisaliu@gmail.com>
2021-12-14 19:55:03 -0500

    updated configs and packages

--- client-app-ui/tailwind.config.js
+++ client-app-ui/tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'), // import tailwind forms
