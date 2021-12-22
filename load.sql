b4372cd9bd34919ea8f9894933c5db91b6306c7a
Jason Liu <jasonisaliu@gmail.com>
2021-12-22 02:00:37 -0500

    completed load data scripts

--- load.sql
+++ load.sql
@@ -6,3 +6,5 @@ LOAD DATA LOCAL INFILE 'var/lib/mysql/PARSED_recipe_ingredients.csv' INTO TABLE 
 LOAD DATA LOCAL INFILE 'var/lib/mysql/PARSED_recipe_tags.csv' INTO TABLE RecipeTags FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (recipeID, tagID);
 LOAD DATA LOCAL INFILE 'var/lib/mysql/PARSED_recipes.csv' INTO TABLE Recipe FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (recipeID, name, authorID, cookTime, date, description);
 LOAD DATA LOCAL INFILE 'var/lib/mysql/PARSED_reviews.csv' INTO TABLE Reviews FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (recipeID, userID, rating, date, review);
+LOAD DATA LOCAL INFILE 'var/lib/mysql/PARSED_users.csv' INTO TABLE Users FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (userID, firstName, lastName, email, phoneNumber, gender, birthday, dateJoined);
+LOAD DATA LOCAL INFILE 'var/lib/mysql/PARSED_tags.csv' INTO TABLE Tags FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS (tagID, tag);
