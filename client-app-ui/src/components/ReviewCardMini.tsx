941452906ea2f6ac92bf42f20690254dd9905a63
Jason Liu <jasonisaliu@gmail.com>
2021-12-23 01:43:54 -0500

    finished delete review

--- client-app-ui/src/components/ReviewCardMini.tsx
+++ client-app-ui/src/components/ReviewCardMini.tsx
@@ -20,6 +20,12 @@ export default function ReviewCardMini(props: Review) {
         ).catch((err) => setHint(err.message))
     }
 
+    const deleteReview = () => {
+        axios.delete(`${baseURL}reviews/${recipeID}/${userID}`).then(res =>
+            window.location.reload()
+        ).catch((err) => setHint(err.message))
+    }
+
     return (
         <div key={review} className="bg-white shadow hover:shadow-lg overflow-hidden sm:rounded-lg">
             {!editMode && <>
@@ -41,7 +47,7 @@ export default function ReviewCardMini(props: Review) {
                         Edit
                     </button>
                     <button
-                        // onClick={() => setEditMode()}
+                        onClick={() => deleteReview()}
                         type="button"
                         className="inline-flex items-center px-4 py-2 border border-red-300 rounded-md shadow-sm text-sm font-medium text-red-700 bg-white hover:bg-gray-50"
                     >
